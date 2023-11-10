16ecc1b6930da1f01aa03685dcfd76829283bac6
prasannakumar <prasannakumar2228@gmail.com>
2023-11-10 13:24:05 -0500

    Remove all files and commit history

--- Professor/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Professor)
-admin.site.register(OfficeHours)
-admin.site.register(Appointment)
--- Professor/signals.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
-from .models import UserProfiles
-from django.dispatch import receiver
