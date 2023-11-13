e02ffe74f8d924d8168f9d3e1f1cb67dd57fdbba
Sagargopu <vidyasagar0389@gmail.com>
2023-11-13 15:47:28 -0500

    emails

--- Student/views.py
+++ Student/views.py
@@ -45,11 +45,14 @@ def BookAppointment(request,pk):
         appointment.Student=student
         appointment.Confirmed=True
         appointment.save()
-        Subject='Appointment Confirmation'
-        Body=f"Your appointment has been scheduled!\n\nThis is to confirm your upcoming appointment with {appointment.Professor.First_Name} {appointment.Professor.Last_Name} scheduled for {appointment.Date} at {appointment.Time} at {appointment.Professor.Office}. Please ensure you are present on time. If you need to reschedule, kindly check my schedule and reschedule the appointment at your earliest convenience.\n\nBest regards,\n{appointment.Professor.First_Name} {appointment.Professor.Last_Name}."
-        send_mail(
-            Subject, Body, settings.EMAIL_HOST_USER, [student.Email], fail_silently=False
-        )
+        try:
+            Subject='Appointment Confirmation'
+            Body=f"Your appointment has been scheduled!\n\nThis is to confirm your upcoming appointment with {appointment.Professor.First_Name} {appointment.Professor.Last_Name} scheduled for {appointment.Date} at {appointment.Time} at {appointment.Professor.Office}. Please ensure you are present on time. If you need to reschedule, kindly check my schedule and reschedule the appointment at your earliest convenience.\n\nBest regards,\n{appointment.Professor.First_Name} {appointment.Professor.Last_Name}."
+            send_mail(
+                Subject, Body, settings.EMAIL_HOST_USER, [student.Email], fail_silently=False
+            )
+        except:
+            return redirect('myappointments')
     return redirect('myappointments')
 
 @login_required(login_url='login')
@@ -59,9 +62,12 @@ def CancelAppointment(request,pk):
     appointment.Student=None
     appointment.Confirmed=False
     appointment.save()
-    Subject='Appointment Cancelled'
-    Body=f"Your appointment has been cancelled!\n\nThis is to confirm you have cancelled upcoming appointment with {appointment.Professor.First_Name} {appointment.Professor.Last_Name} on {appointment.Date} at {appointment.Time} at {appointment.Professor.Office}. If you need to reschedule, kindly check my schedule and reschedule the appointment at your convenience.\n\nBest regards,\n{appointment.Professor.First_Name} {appointment.Professor.Last_Name}."
-    send_mail(
-            Subject, Body, settings.EMAIL_HOST_USER, [student.Email], fail_silently=False
-        )
-    return redirect('myappointments')
+    try:
+        Subject='Appointment Cancelled'
+        Body=f"Your appointment has been cancelled!\n\nThis is to confirm you have cancelled upcoming appointment with {appointment.Professor.First_Name} {appointment.Professor.Last_Name} on {appointment.Date} at {appointment.Time} at {appointment.Professor.Office}. If you need to reschedule, kindly check my schedule and reschedule the appointment at your convenience.\n\nBest regards,\n{appointment.Professor.First_Name} {appointment.Professor.Last_Name}."
+        send_mail(
+                Subject, Body, settings.EMAIL_HOST_USER, [student.Email], fail_silently=False
+            )
+        return redirect('myappointments')
+    except:
+        return redirect('myappointments')
